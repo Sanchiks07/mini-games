212e2f768efc4f166b65cacde647a8e983d98100
tin.keita <ipb23.t.paegle@vtdt.edu.lv>
2025-11-12 10:49:50 +0200

    mainīts css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,13 @@
 <x-layout>
-    <h2>Login</h2>
-    <form action="/login" method="POST">
-        @csrf
-        <label>
-            E-mail:
-            <input name="email" id="email" type="email"/>
-        </label><br><br>
-        <label>
-            Password:
-            <input name="password" id="password" type="password"/>
-        </label><br><br>
-        <button>Login</button>
-    </form>
-    <p>Don't have an account?</p>
-    <a href="{{ route('register') }}">Register</a>
+    <div class="login">
+        <h2>Login</h2>
+        <form action="/login" method="POST" class="login-form">
+            @csrf
+            <input name="email" id="email" type="email" placeholder="E-mail..."/>
+            <input name="password" id="password" type="password" placeholder="Password..."/>
+            <button class="small-pretty">Login</button>
+        </form>
+        <p>Don't have an account?</p>
+        <a href="{{ route('register') }}">Register</a>
+    </div>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,34 +1,25 @@
 <x-layout>
-    <h2>Register</h2>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="/register" method="POST">
-        @csrf
-        <label>
-            Name:
-            <input name="name" id="name" type="text" value="{{ old("name") }}"/>
-        </label><br><br>
-        <label>
-            E-mail:
-            <input name="email" id="email" type="email" value="{{ old("email") }}"/>
-        </label><br><br>
-        <label>
-            Password:
-            <input name="password" id="password" type="password"/>
-        </label><br><br>
-        <label>
-            Password confirmation:
-            <input name="password_confirmation" id="password_confirmation" type="password"/>
-        </label><br><br>
-        <button>Register</button>
-    </form>
-    <p>Already have an account?</p>
-    <p><a href="{{ route('login.form') }}">Login</a></p>
+    <div class="login">
+        <h2>Register</h2>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="/register" method="POST" class="login-form">
+            @csrf
+            <input name="name" id="name" type="text" placeholder="Name..." value="{{ old("name") }}"/>
+            <input name="email" id="email" type="email" placeholder="E-mail..." value="{{ old("email") }}"/>
+            <input name="password" id="password" type="password" placeholder="Password..."/>
+            <input name="password_confirmation" id="password_confirmation" type="password" placeholder="Password confirmation..."/>
+            <button class="small-pretty">Register</button>
+        </form>
+        <p>Already have an account?</p>
+        <p><a href="{{ route('login.form') }}">Login</a></p>
+    </div>
+    
 </x-layout>
